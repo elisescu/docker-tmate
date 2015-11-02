3cf9bb9279652b2eecb8ae3ca465b627a7d08e8d
Vasile Popescu <elisescu@elisescu.com>
2015-11-02 23:45:47 +0200

    change the message to use the env vars for host and port

--- message.sh
+++ message.sh
@@ -4,8 +4,8 @@ DSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_dsa_key 2>&1 | cut -d\  -f 2`
 RSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_rsa_key 2>&1 | cut -d\  -f 2`
 ECDSA=`ssh-keygen -l -f /tmate-slave/keys/ssh_host_ecdsa_key 2>&1 | cut -d\  -f 2`
 echo Add this to your ~/.tmate.conf file
-echo set -g tmate-server-host "<server ip/hostname>"
-echo set -g tmate-server-port \<server port\>
+echo set -g tmate-server-host "$HOST"
+echo set -g tmate-server-port \<$PORT\>
 echo set -g tmate-server-rsa-fingerprint   \"$RSA\"
 echo set -g tmate-server-dsa-fingerprint   \"$DSA\"
 echo set -g tmate-server-ecdsa-fingerprint   \"$ECDSA\"
